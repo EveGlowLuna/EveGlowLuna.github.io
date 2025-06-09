80a3af705680820541f081c303f315707d9f28a3
EveGlowLuna <yy15502497613@outlook.com>
2025-06-09 22:59:33 +0800

    修复路由

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -13,8 +13,8 @@ export default navbar([
           text: "快速上手",
           prefix: "/getstarted/",
           children: [
-            { text: "介绍", icon: "flag", link: "/getstarted/getstarted/" },
-            { text: "基础操作", icon: "flag", link: "/getstarted/getstarted/getstarted" },
+            { text: "介绍", icon: "flag", link: "/getstarted/" },
+            { text: "基础操作", icon: "flag", link: "/getstarted/getstarted" },
             { text: "进阶操作", icon: "rocket", link: "/getstarted/advance" }
           ]
         }
