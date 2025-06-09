a748726666cdb8e288bc884c3133a365bfc94c71
EveGlowLuna <yy15502497613@outlook.com>
2025-06-09 11:33:00 +0800

    更新“编辑此页”的重定向

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/EveGlowLuna/EveGlowLuna.github.io/tree/gh-pages',
         },
         blog: {
           showReadingTime: false,
