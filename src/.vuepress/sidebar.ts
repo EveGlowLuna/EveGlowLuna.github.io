70295e656675c9bf2401bbe8f39445a825c0dbb0
EveGlowLuna <yy15502497613@outlook.com>
2025-06-10 00:12:46 +0800

    修复路由

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -15,8 +15,8 @@ export default sidebar({
           text: "快速上手",
           prefix: "/getstarted/",
           children: [
-            { text: "介绍", icon: "flag", link: "/getstarted/getstarted/" },
-            { text: "基础操作", icon: "flag", link: "/getstarted/getstarted/getstarted" },
+            { text: "介绍", icon: "flag", link: "/getstarted/" },
+            { text: "基础操作", icon: "flag", link: "/getstarted/getstarted" },
             { text: "进阶操作", icon: "rocket", link: "/getstarted/advance" }
           ]
         }
